3bf8d58fe7c4939b12e1f7d7428b40c7ea00c6d4
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-20 00:52:09 -0600

    feat(root): Create song ticker compontet to show song title

---
+++ src/components/SongTicker/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.css";
+import MusicIcon from "../icons/MusicIcon";
+import Marquee from "react-fast-marquee";
+export default function SongTicker({ songTitle }) {
+  return (
+    <div className={styles.song}>
+      <MusicIcon />
+      <Marquee gradient={false} speed={100}>
+        {songTitle}
+      </Marquee>
+    </div>
+  );
+}
