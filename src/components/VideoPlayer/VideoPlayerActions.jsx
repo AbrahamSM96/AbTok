2b3fe824e3ca1fe1722870e9308d60d7e3d33791
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-19 19:48:37 -0600

    Add(addicons): add icons in principal view

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { Heart } from "../../components/icons/Heart.jsx";
-export default function VideoPlayerActions() {
+import { Share } from "../../components/icons/Share.jsx";
+import { Comment } from "../../components/icons/Comment.jsx";
+export default function VideoPlayerActions({
+  likes = 2041,
+  comments = 333,
+  shares = 13,
+  hearted = false,
+}) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title="like">{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title="comments">{comments}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title="shares">{shares}</strong>
       </div>
     </aside>
   );
