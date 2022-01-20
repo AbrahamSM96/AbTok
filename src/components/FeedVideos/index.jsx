1a2f03b18c2b4ad98ba1220b3e4edcadda14be02
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-20 00:54:49 -0600

    feat(root): add new resulto to the feed

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -10,8 +10,9 @@ const VIDEOS = [
     shares: 345,
     comments: 33,
     songTitle: "song-pedrito",
-    albumCover: "",
-    src: "https://v16-webapp.tiktok.com/46754d584e8e56a9a48e946888bf15ac/61e909a7/video/tos/maliva/tos-maliva-ve-0068c799-us/798f2f0087a24cb1a89181bb647d6372/?a=1988&br=2588&bt=1294&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs7eTs6ZndnOTMzZzczNEApOTg2Ozo4N2U1NzYzZzU4Z2dmYTVlcjQwYjJgLS1kMS9zcy1iMF9hXmE0MDYuMC9fMWI6Yw%3D%3D&vl=&vr=",
+    albumCover:
+      "https://i.pinimg.com/originals/aa/22/0d/aa220d9cf0029f53689d030c4aa44a37.jpg",
+    src: "https://v16-webapp.tiktok.com/3d0ad4e3073ac3492c66af3f38b78759/61e90963/video/tos/useast2a/tos-useast2a-ve-0068c003/dda3652a0aac4af3a9ed185f55aa5764/?a=1988&br=1632&bt=816&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs6cjY6Zmc6OjMzNzczM0ApPDRnNWdlOztoN2c7OjNmM2draC5gcjRnMmhgLS1kMTZzc15hYTE2LS8tNl5fX2IwNTI6Yw%3D%3D&vl=&vr=",
   },
   {
     id: 2,
@@ -21,8 +22,9 @@ const VIDEOS = [
     shares: 345,
     comments: 33,
     songTitle: "song-pedrito",
-    albumCover: "",
-    src: "https://v16-webapp.tiktok.com/46754d584e8e56a9a48e946888bf15ac/61e909a7/video/tos/maliva/tos-maliva-ve-0068c799-us/798f2f0087a24cb1a89181bb647d6372/?a=1988&br=2588&bt=1294&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs7eTs6ZndnOTMzZzczNEApOTg2Ozo4N2U1NzYzZzU4Z2dmYTVlcjQwYjJgLS1kMS9zcy1iMF9hXmE0MDYuMC9fMWI6Yw%3D%3D&vl=&vr=",
+    albumCover:
+      "https://e.snmc.io/i/600/s/1b7f901dc631d6517431f26076abd33a/8937940/panda-amantes-sunt-amentes-Cover-Art.jpg",
+    src: "https://v16-webapp.tiktok.com/3d0ad4e3073ac3492c66af3f38b78759/61e90963/video/tos/useast2a/tos-useast2a-ve-0068c003/dda3652a0aac4af3a9ed185f55aa5764/?a=1988&br=1632&bt=816&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs6cjY6Zmc6OjMzNzczM0ApPDRnNWdlOztoN2c7OjNmM2draC5gcjRnMmhgLS1kMTZzc15hYTE2LS8tNl5fX2IwNTI6Yw%3D%3D&vl=&vr=",
   },
   {
     id: 3,
@@ -32,8 +34,9 @@ const VIDEOS = [
     shares: 345,
     comments: 33,
     songTitle: "song-pedrito",
-    albumCover: "",
-    src: "https://v16-webapp.tiktok.com/46754d584e8e56a9a48e946888bf15ac/61e909a7/video/tos/maliva/tos-maliva-ve-0068c799-us/798f2f0087a24cb1a89181bb647d6372/?a=1988&br=2588&bt=1294&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs7eTs6ZndnOTMzZzczNEApOTg2Ozo4N2U1NzYzZzU4Z2dmYTVlcjQwYjJgLS1kMS9zcy1iMF9hXmE0MDYuMC9fMWI6Yw%3D%3D&vl=&vr=",
+    albumCover:
+      "https://upload.wikimedia.org/wikipedia/en/9/91/Panda-Para_Ti_Con_Desprecio-Fronta.jpg",
+    src: "https://v16-webapp.tiktok.com/3d0ad4e3073ac3492c66af3f38b78759/61e90963/video/tos/useast2a/tos-useast2a-ve-0068c003/dda3652a0aac4af3a9ed185f55aa5764/?a=1988&br=1632&bt=816&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=202201200103530102230650220D179F72&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzs6cjY6Zmc6OjMzNzczM0ApPDRnNWdlOztoN2c7OjNmM2draC5gcjRnMmhgLS1kMTZzc15hYTE2LS8tNl5fX2IwNTI6Yw%3D%3D&vl=&vr=",
   },
 ];
 
